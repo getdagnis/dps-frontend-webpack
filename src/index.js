141c7775c1d8eae42221b6545f3adb52673695a8
Dagnis Skurbe <dagnis.skurbe@gmail.com>
2021-04-22 14:55:39 +0300

    small ui improvements

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import { homepage } from "./containers";
 
 document.querySelector("app").innerHTML = '<div class="app"></div>';
 document.querySelector(".app").innerHTML = homepage;
+
+const paymentButton = document.querySelector(
+  ".dps-dashboard__right__top__button"
+);
+const statusRed = document.querySelector(".status-red");
+
+paymentButton.addEventListener("click", (e) => {
+  statusRed.classList.replace("status-red", "status-green");
+  statusRed.innerHTML = "All due payments made";
+  paymentButton.innerHTML = "Pay in advance";
+});
--- src/styles/index.js
+++ src/styles/index.js
@@ -2,6 +2,6 @@ import * as navbarStyles from "./navbar.scss";
 import * as globalStyles from "./global.scss";
 import * as dashboardStyles from "./dashboard.scss";
 
-const styles = navbarStyles + globalStyles + dashboardStyles;
+const styles = navbarStyles + dashboardStyles + globalStyles;
 
 export default styles;
